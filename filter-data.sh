cd4043e35dea2ecdaa7f08fbfe31ef7ff0c56b45
Martin Basti <mbasti@redhat.com>
2023-02-06 15:14:02 +0100

    fix filter
    Signed-off-by: Martin Basti <mbasti@redhat.com>

--- filter-data.sh
+++ filter-data.sh
@@ -9,6 +9,6 @@ authors='[
 jq --argjson authors "${authors}" -r '[
     .[] | select(
         ([.user] | inside($authors)) or
-        (.url | contains("/test-team/")) or
+        (.url | contains("/test-team/"))
     )
 ]'
