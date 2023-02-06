371f4b53dadd10b5e8a06bfe435d7b37e1eed405
Martin Basti <mbasti@redhat.com>
2023-02-06 14:54:26 +0100

    Update to test_team part 1
    Signed-off-by: Martin Basti <mbasti@redhat.com>

--- filter-data.sh
+++ filter-data.sh
@@ -2,20 +2,13 @@
 set -euo pipefail
 
 authors='[
-  "cuipinghuo",
-  "joejstuart",
-  "lcarva",
-  "robnester-rh",
-  "simonbaird",
-  "zregvart",
-  "ec-automation[bot]"
+  "martinbasti"
 ]'
 
 # Assumes data from stdin!
 jq --argjson authors "${authors}" -r '[
     .[] | select(
         ([.user] | inside($authors)) or
-        (.url | contains("/hacbs-contract/")) or
-        (.title | test("enterprise.contract"; "i"))
+        (.url | contains("/test-team/")) or
     )
 ]'
