3db5354578b412aca23bdfeeff43ef076f44fe7d
Martin Basti <mbasti@redhat.com>
2023-11-16 14:40:00 +0100

    Report autoupdate bots only in subset of repos
    Signed-off-by: Martin Basti <mbasti@redhat.com>

--- filter-data.sh
+++ filter-data.sh
@@ -12,15 +12,23 @@ authors='[
   "sonam1412",
   "jsztuka",
   "14rcole",
-  "hacbs-test-bot",
+  "hacbs-test-bot"
+]'
+
+bots='[
   "dependabot[bot]",
   "renovate[bot]"
 ]'
 
+
 # Assumes data from stdin!
-jq --argjson authors "${authors}" -r '[
+jq --argjson authors "${authors}" --argjson bots "${bots}" -r '[
     .[] | select(
         ([.user] | inside($authors)) or
-        (.url | contains("/test-team/"))
+        (.url | contains("/test-team/")) or
+        (
+	  ([.user] | inside($bots)) and
+	  (.url | test ("build-definitions|integration-service|integration-examples|hacbs-test|clair-in-ci-db"))
+        )
     )
 ]'
